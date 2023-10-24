5fc33900101933473f709416d4f32395f488fb6c
DestructiveSillyonker <johnr636@deltalearns.ca>
2023-10-24 13:10:12 -0700

    Done

--- task1.py
+++ task1.py
@@ -8,3 +8,29 @@ The program will then tell the user if the number is too high, too low or correc
 If the number is correct, the program will end
 If the 10 guesses are used up, the program will say that the user has lost
 '''
+import random
+Number = random.randint(1,100)
+
+
+input("------------------------Lets play a game------------------------\n---------------------Press enter to continue--------------------")
+print("     ")
+print("     ")
+print("     ")
+print("     ")
+print("     ")
+print("     ")
+print("     ")
+for i in range(1,11):
+    Guess = int(input("Geuss a number from 1-100: "))
+
+    if Guess > Number:
+        print(f"{Guess} is Cold, Try lower")
+
+    if Guess < Number:
+        print(f"{Guess} is Cold, Try higher")
+
+    if Guess == Number:
+        print(f"{Guess} is Correct!")
+        break
+else:
+    print(f"{Number} was the number, come on that was easy!")
--- task2.py
+++ task2.py
@@ -7,5 +7,19 @@ If they are not correct, the program will say "Access Denied".
 There will be a maximum of 3 guesses allowed
 '''
 
-expectedUsername = "systemadmin"
-expectedPassword = "master"
+expUser = "systemadmin"
+expPass = "master"
+
+User = input("welcome, please enter your User and password, User: ")
+for i in range(1,4):
+    if User == expUser:
+        Pass = input(f"{expUser} is Correct, enter password: ")
+        for i in range(1,4):
+            if Pass == expPass:
+                print(f"{expUser} and {expPass}")
+                break
+            else:
+                print("get out of my pc :[")
+        break        
+    else:
+        print("get out of my pc :[")
